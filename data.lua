155bbef9e2cd3d3db3ae43f56327386a3e827b9f
MrSmoothieHuman1 <stnlwbbr@gmail.com>
2025-08-25 21:28:12 +0100

    added fish items

--- data.lua
+++ data.lua
@@ -1,1 +1,2 @@
 require("prototypes.entity.fish")
+require("prototypes.item")
---
+++ prototypes/item.lua
@@ -0,0 +1,17 @@
+data:extend({
+
+    {
+        type = "item",
+        name = "be-iron-fish",
+        icon_size = 64,
+        icon = "__base__/graphics/icons/fish.png",
+        stack_size = 100
+    },
+    {
+        type = "item",
+        name = "be-copper-fish",
+        icon_size = 64,
+        icon = "__base__/graphics/icons/fish.png",
+        stack_size = 100
+    },
+})
